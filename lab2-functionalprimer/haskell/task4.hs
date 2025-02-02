969c7a07585e3bd9eba10dabe46492521e5550a3
Amat Bittaye <abittaye84@gmail.com>
2025-02-02 16:23:02 +0000

    week 2 lab

--- lab2-functionalprimer/haskell/task4.hs
+++ lab2-functionalprimer/haskell/task4.hs
@@ -1,20 +1,17 @@
-ask :: String -> Int -> IO ()
-ask prompt count =
-  do
-    -- Append "!" to the prompt based on the count
-    let modifiedPrompt = prompt ++ replicate count '!'
-    putStrLn modifiedPrompt
-    line <- getLine
-    if line == "quit"
-      then putStrLn "quitting..."
-      else if line == ""
-        then ask prompt (count + 1)  -- Increment the count for consecutive empty inputs
+ask :: String -> IO ()
+ask prompt =
+    do
+        putStrLn prompt
+        line <- getLine
+        if line == ""
+            then ask (prompt ++ "!")  -- append ! to prompt each time empty string entered
+        else if (line == "quit") then putStrLn "quitting..."
         else do
-          putStrLn ("you said: " ++ reverse line)
-          ask prompt 0  -- Reset the count after a non-empty input
+            putStrLn ("you said: " ++ reverse line)
+            ask prompt
 
 main :: IO ()
 main =
-  do
-    let prompt = "please say something"
-    ask prompt 0  -- Start with 0 "!"s
+    do
+        let prompt = "please say something"
+        ask prompt

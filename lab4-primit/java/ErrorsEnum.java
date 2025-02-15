58bd3a3a5107700ff41731dbf3e33911a41c9723
Amat Bittaye <abittaye84@gmail.com>
2025-02-15 17:13:02 +0000

    LAB 4

--- lab4-primit/java/ErrorsEnum.java
+++ lab4-primit/java/ErrorsEnum.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+/*import java.util.Scanner;
 import java.util.EnumSet;
 
 public class ErrorsEnum
@@ -65,5 +65,75 @@ public class ErrorsEnum
         
         Error e = getEnumElement("error", Error.class);
         System.out.println(e + " results in: " + error2Result(e));
+    }
+}
+*/
+
+import java.util.EnumSet;
+
+public class ErrorsEnumSolution
+{
+    enum Error { FP_ROUNDING, FP_OVERFLOW, FP_UNDERFLOW, INT_OVERFLOW }
+
+    enum Result { A_BIT_DIFFERENT, INFINITY, ZERO, VERY_DIFFERENT }
+    
+    private static <E extends Enum<E>> E getEnumElement(String elementTypeName, Class<E> elementType)
+    {
+        boolean haveResult = false;
+        E result = null;
+        Scanner stdin = new Scanner(System.in);
+        
+        while ( ! haveResult )
+        {
+            System.out.print("Input " + elementTypeName + ": ");
+            try
+            {
+                result = Enum.valueOf(elementType, stdin.next().toUpperCase());
+                haveResult = true;
+            }
+            catch (IllegalArgumentException e)
+            {
+                System.out.println("Not a valid " + elementTypeName + ".");
+                stdin.nextLine(); // skip the invalid input
+            }
+        }
+        
+        stdin.close();
+        return result;
+    }
+  
+  private static Error result2Error(Result e)
+    {
+        Error error = null;
+        
+        switch (e) {
+        case A_BIT_DIFFERENT:
+            error = Error.FP_ROUNDING;
+            break;
+        case INFINITY:
+            error = Error.FP_OVERFLOW;
+            break;
+        case ZERO:
+            error = Error.FP_UNDERFLOW;
+            break;
+        case VERY_DIFFERENT:
+            error = Error.INT_OVERFLOW;
+            break;
+        }
+        
+        return error;
+    }
+
+    public static void main(String[] args)
+    {
+       System.out.print("Known results = ");
+       for (Result r : EnumSet.allOf(Result.class))
+        {
+            System.out.print(r + " ");
+        }
+        System.out.println();
+        
+        Result r = getEnumElement("result", Result.class);
+        System.out.println(r + " results from: " + result2Error(r));
     }
 }
--- lab4-primit/javascript/ErrorsEnum.js
+++ lab4-primit/javascript/ErrorsEnum.js
@@ -1,4 +1,4 @@
-const prompt=require("prompt-sync")({sigint:true}); 
+/*const prompt=require("prompt-sync")({sigint:true}); 
 
 const Error_enumobj = {
 	FP_ROUNDING: "FP_ROUNDING",
@@ -45,5 +45,56 @@ while(!validArg){
     }
     else{
         console.log(result);
+    }
+}
+*/
+
+const prompt=require("prompt-sync")({sigint:true}); 
+
+const Error_enumobj = {
+	FP_ROUNDING: "FP_ROUNDING",
+	FP_OVERFLOW: "FP_OVERFLOW",
+	FP_UNDERFLOW: "FP_UNDERFLOW",
+	INT_OVERFLOW: "INT_OVERFLOW"
+}
+
+const Result_enumobj = {
+    A_BIT_DIFFERENT: "A_BIT_DIFFERENT", 
+    INFINITY: "INFINITY", 
+    ZERO: "ZERO", 
+    VERY_DIFFERENT: "VERY_DIFFERENT"
+}
+
+function result2Error(result){
+	 switch (result) {
+	 case Result_enumobj.A_BIT_DIFFERENT:
+		return Error_enumobj.FP_ROUNDING;
+	break;
+	case Result_enumobj.INFINITY:
+	    return Error_enumobj.FP_OVERFLOW;	
+	break;
+	case Result_enumobj.ZERO:
+	    return Error_enumobj.FP_UNDERFLOW;	
+	break;
+	case  Result_enumobj.VERY_DIFFERENT:
+	    return Error_enumobj.INT_OVERFLOW;	
+	break;
+	default:
+		return 'Invalid Result value';
+}
+
+}
+
+console.log('Result list: ', Object.values(Result_enumobj));
+var validArg = false;
+while(!validArg){
+    var input = prompt("Input: ");
+    let err = result2Error(input);
+	 if (Object.values(Error_enumobj).includes(err)){
+        validArg = true;
+		console.log(input + " results from " + result2Error(input));
+    }
+    else{
+        console.log(err);
     }
 }
